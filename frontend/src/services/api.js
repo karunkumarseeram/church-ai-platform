3a91dc13fb9181716699f6a71a2b65d9cf07e633
KK <karunkumarseeram@gmail.com>
2026-07-05 09:43:09 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "http://localhost:8000", // FastAPI backend
   // withCredentials: true, // only if you plan to send cookies
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000", // FastAPI backend
+  baseURL: import.meta.env.VITE_API_URL,// FastAPI backend
 });
 
 // attach token automatically
