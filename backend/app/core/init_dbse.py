5971e60149fd943e3b7ca1cac6e65799590c19f5
KK <karunkumarseeram@gmail.com>
2026-08-02 20:58:50 +0530

    commit prod to fix tables

--- backend/app/core/init_dbse.py
+++ backend/app/core/init_dbse.py
@@ -1,6 +1,19 @@
 from app.core.database import Base, engine
 
+
 def init_db():
-    # from app.models.chr_models import User, Member, Event, Donation, Verse, PrayerRequest, AdminActionLog
-    from app.models.chr_models import User, Member, Event, EventAttendance, Donation, Verse, BibleVerse, PrayerRequest, AdminActionLog, OTPS
+    from app.models.chr_models import (
+        User,
+        Member,
+        Event,
+        EventAttendance,
+        Donation,
+        Verse,
+        BibleVerse,
+        PrayerRequest,
+        AdminActionLog,
+        OTPS,
+        Feedback,
+        FeedbackReply,
+    )
     Base.metadata.create_all(bind=engine)
